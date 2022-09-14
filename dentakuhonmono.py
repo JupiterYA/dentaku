deff69f62e7c951c0d745619cee1eedcf9f11100
JupiterYA <109979749+JupiterYA@users.noreply.github.com>
2022-09-15 01:02:59 +0900

    Create dentakuhonmono.py

---
+++ dentakuhonmono.py
@@ -0,0 +1,19 @@
+import PySimpleGUI as sg
+
+layout = [ 
+            [sg.Button('AC'), sg.Button('   '), sg.Button('   '), sg.Button(' / '),],
+            [sg.Button(' 7 '), sg.Button(' 8 '), sg.Button(' 9 '), sg.Button(' * '),],
+            [sg.Button(' 4 '), sg.Button(' 5 '), sg.Button(' 6 '), sg.Button(' - '),],
+            [sg.Button(' 1 '), sg.Button(' 2 '), sg.Button(' 3 '), sg.Button(' + '),],
+            [sg.Button(' 0 '), sg.Button(' . '), sg.Button('   '), sg.Button(' = '),] ]
+
+window = sg.Window('電卓',layout)
+
+while True:
+
+    event, values = window.read()
+    # ユーザーが終了したいのか、ウィンドウが閉じられたかどうかを確認してください
+    if event == sg.WINDOW_CLOSED:
+        break
+
+window.close()
